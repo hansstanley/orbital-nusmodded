3fd14d4b3b063cbcac821a6af4757cdea0a4ed60
hansstanley <nusstuhss@gmail.com>
2022-06-05 12:17:00 +0800

    Add "memoization" for nusmods integration

--- backend/src/nusmods/nusmods.service.ts
+++ backend/src/nusmods/nusmods.service.ts
@@ -9,15 +9,27 @@ const ACAD_YEAR = '2021-2022';
 
 @Injectable()
 export class NusmodsService {
+  private summaries: ModSummaryDto[] = undefined;
+  private details: ModDetailDto[] = undefined;
+  private modInfo = new Map<string, ModInfoDto>();
+
   constructor(private httpService: HttpService) { }
 
+  /**
+   * Retrieves the list of module summaries from NUSMods.
+   * 
+   * @returns An array of module summaries.
+   */
   async getSummaries(): Promise<ModSummaryDto[]> {
+    if (this.summaries) return this.summaries;
+
     const moduleList = await lastValueFrom(this.httpService
       .get(`${NUSMODS_API_URL}/${ACAD_YEAR}/moduleList.json`)
       .pipe(map((res) => res.data)));
 
     if (Array.isArray(moduleList)) {
-      return moduleList.map((mod) => plainToInstance(ModSummaryDto, mod));
+      this.summaries = moduleList.map((mod) => plainToInstance(ModSummaryDto, mod));
+      return this.summaries;
     } else {
       throw new HttpException(
         `Unable to retrieve module summaries`,
@@ -26,13 +38,21 @@ export class NusmodsService {
     }
   }
 
+  /**
+   * Retrieves the list of detailed module information from NUSMods.
+   * 
+   * @returns An array of module details.
+   */
   async getDetails(): Promise<ModDetailDto[]> {
+    if (this.details) return this.details;
+
     const moduleList = await lastValueFrom(this.httpService
       .get(`${NUSMODS_API_URL}/${ACAD_YEAR}/moduleInfo.json`)
       .pipe(map((res) => res.data)));
 
     if (Array.isArray(moduleList)) {
-      return moduleList.map((mod) => plainToInstance(ModDetailDto, mod));
+      this.details = moduleList.map((mod) => plainToInstance(ModDetailDto, mod));
+      return this.details;
     } else {
       throw new HttpException(
         `Unable to retrieve module details`,
@@ -41,7 +61,16 @@ export class NusmodsService {
     }
   }
 
+  /**
+   * Retrieves the comprehensive module information
+   * for a given module code.
+   * 
+   * @param moduleCode The module code of the desired module.
+   * @returns The comprehensive module information.
+   */
   async getModuleInfo(moduleCode: string): Promise<ModInfoDto> {
+    if (this.modInfo.has(moduleCode)) return this.modInfo.get(moduleCode);
+
     const res = await lastValueFrom(this.httpService
       .get(`${NUSMODS_API_URL}/${ACAD_YEAR}/modules/${moduleCode}.json`)
       .pipe(catchError((err) => of(err.response))));
@@ -53,6 +82,7 @@ export class NusmodsService {
       );
     }
 
-    return plainToInstance(ModInfoDto, res.data);
+    this.modInfo.set(moduleCode, plainToInstance(ModInfoDto, res.data));
+    return this.modInfo.get(moduleCode);
   }
 }
