6c1bef9b5bbc2e39c87e0512228d6364a84ed364
Tan Yuan Wei <tyw2811@gmail.com>
2022-06-07 01:57:15 +0800

    Added a dialog for the modules in the roadmap

--- website/src/pages/Roadmap/ModuleBox.jsx
+++ website/src/pages/Roadmap/ModuleBox.jsx
@@ -1,8 +1,19 @@
-import { Card, CardContent, Skeleton, Typography } from "@mui/material";
+import * as React from 'react';
+import { Card, CardActionArea, CardContent, Dialog, Skeleton, Typography } from "@mui/material";
 import { ModuleInfoContext } from "../../contexts";
+
 
 export default function ModuleBox(props) {
   const { moduleCode } = props;
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const generateContent = (moduleMap, isLoaded) => {
     const moduleInfo = isLoaded
@@ -13,15 +24,48 @@ export default function ModuleBox(props) {
           title: <Skeleton />,
         };
     return moduleInfo ? (
-      <Card sx={{ minWidth: 240 }}>
-        <CardContent>
-          <Typography variant="caption">
-            {moduleInfo.moduleCredit} MC
-          </Typography>
-          <Typography variant="subtitle1">{moduleInfo.moduleCode}</Typography>
-          <Typography variant="body2">{moduleInfo.title}</Typography>
-        </CardContent>
-      </Card>
+      <>
+        <Card>
+          <CardActionArea onClick={handleClickOpen}>
+            <CardContent sx={{ width: 340, height: 90 }}>
+              <Typography variant="caption" position='relative' bottom='5%'>
+                {moduleInfo.moduleCredit} MCs
+              </Typography>
+              <Typography variant="subtitle1" position='relative' bottom='5%'>
+                {moduleInfo.moduleCode}
+              </Typography>
+              <Typography variant="body2" position='relative' bottom='10%' noWrap >
+                {moduleInfo.title}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Dialog onClose={handleClose} open={open} maxWidth = 'lg'>
+          <Card sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              m: 0,
+              width: 'fit-content',
+              padding: 2,
+            }}>
+            <Typography variant="body2">
+              {moduleInfo.department + ", " + moduleInfo.faculty}
+            </Typography>
+            <Typography variant="body">
+              {moduleInfo.moduleCredit} MCs
+            </Typography>
+            <Typography variant="h4">
+              {moduleInfo.moduleCode}
+            </Typography>
+            <Typography variant="h6">
+              {moduleInfo.title}
+            </Typography>
+            <Typography variant="subtitle1">
+                {moduleInfo.description !== "" ? moduleInfo.description : "This module does not have a module description."}
+            </Typography>
+          </Card>
+        </Dialog>
+      </>
     ) : null;
   };
 
