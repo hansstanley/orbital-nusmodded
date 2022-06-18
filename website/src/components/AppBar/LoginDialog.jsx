cc7d356fc189b480aca16096da6ba474dae69c6d
hansstanley <nusstuhss@gmail.com>
2022-06-18 16:49:38 +0800

    Update login dialog
    Rename AccountDetails.jsx

--- website/src/components/AppBar/LoginDialog.jsx
+++ website/src/components/AppBar/LoginDialog.jsx
@@ -1,114 +1,188 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   Button,
   Dialog,
-  Typography,
-  Card,
   TextField,
-  Stack,
   Box,
-  Link,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  useMediaQuery,
+  useTheme,
+  Collapse,
+  Alert,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../../services";
 
 export default function LoginDialog({ open, handleClose }) {
   const navigate = useNavigate();
+  const [loading, setLoading] = React.useState(false);
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [validateInput, setValidateInput] = React.useState("");
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleCloseReset = () => {
     setEmail("");
     setPassword("");
     setValidateInput("");
     handleClose();
-  }
+  };
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
-  }
+  };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
-  }
+  };
 
   const handleSubmit = async (event) => {
+    setLoading(true);
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const { user, session, error } = await supabase.auth.signIn({
-        email: data.get('email'),
-        password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
-    console.log(error);
+
     if (error === null) {
       setValidateInput("");
       handleCloseReset();
       navigate("/");
     } else {
-      setValidateInput("Invalid login credentials!");
+      setValidateInput(error.message);
     }
-  }
+    setLoading(false);
+  };
 
   return (
-    <Dialog open={open} handleClose={handleClose}>
-      <Card sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                m: 0,
-                width: 'fit-content',
-                padding: 4,
-                paddingBottom: 10,
-                paddingTop: 8,
-              }}>
-        <Box component="form" onSubmit={handleSubmit} >
-          <Stack spacing = {2}>
+    <Dialog open={open} handleClose={handleClose} fullScreen={fullScreen}>
+      <Box component="form" onSubmit={handleSubmit}>
+        <DialogTitle>Login</DialogTitle>
+        <DialogContent>
+          <DialogContentText
+            variant="h3"
+            color="primary"
+            sx={{ fontWeight: "bold" }}
+          >
+            NUSMODDED
+          </DialogContentText>
+          <TextField
+            id="email"
+            name="email"
+            label="Email"
+            type="email"
+            margin="dense"
+            autoFocus
+            fullWidth
+            required
+            defaultValue={email}
+            onChange={handleEmailChange}
+          />
+          <TextField
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            margin="dense"
+            fullWidth
+            required
+            defaultValue={password}
+            onChange={handlePasswordChange}
+          />
+          <Button size="small">Forgot password</Button>
+          <Collapse in={!!validateInput}>
+            <Alert severity="error">{validateInput}</Alert>
+          </Collapse>
+        </DialogContent>
+        <DialogActions>
+          <Button type="submit" variant="contained" disabled={loading}>
+            Login
+          </Button>
+          <Button onClick={handleCloseReset}>Cancel</Button>
+        </DialogActions>
+      </Box>
+      {/* <Card
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          m: 0,
+          width: "fit-content",
+          padding: 4,
+          paddingBottom: 10,
+          paddingTop: 8,
+        }}
+      >
+        <Box component="form" onSubmit={handleSubmit}>
+          <Stack spacing={2}>
             <Box>
-              <Typography align="center" variant="h3" paddingLeft={2} paddingRight={2} color="primary">
+              <Typography
+                align="center"
+                variant="h3"
+                paddingLeft={2}
+                paddingRight={2}
+                color="primary"
+              >
                 NUSMODDED
               </Typography>
-              <Typography align="center" variant="h5" paddingLeft={2} paddingRight={2} paddingBottom={2}>
+              <Typography
+                align="center"
+                variant="h5"
+                paddingLeft={2}
+                paddingRight={2}
+                paddingBottom={2}
+              >
                 Login
               </Typography>
             </Box>
-            <Stack></Stack><Stack></Stack>
+            <Stack></Stack>
+            <Stack></Stack>
             <TextField
               label="Email"
-              name = "email"
-              defaultValue= {email}
+              name="email"
+              defaultValue={email}
               size="small"
               required
-              onChange = {handleEmailChange}
+              onChange={handleEmailChange}
             />
             <Stack>
-            <TextField
-              type="password"
-              label="Password"
-              name = "password"
-              defaultValue= {password}
-              size="small"
-              required
-              onChange = {handlePasswordChange}
-            />
-            <Link>
-              <Typography variant="caption" paddingLeft = {0.2}>
-                Forget Password?
-              </Typography>
-            </Link>
+              <TextField
+                type="password"
+                label="Password"
+                name="password"
+                defaultValue={password}
+                size="small"
+                required
+                onChange={handlePasswordChange}
+              />
+              <Link>
+                <Typography variant="caption" paddingLeft={0.2}>
+                  Forget Password?
+                </Typography>
+              </Link>
             </Stack>
             <Stack></Stack>
-            <Stack direction = "row" spacing = {2} justifyContent="space-evenly">
-              <Button variant="contained" autoFocus type="submit" size = 'large'>
+            <Stack direction="row" spacing={2} justifyContent="space-evenly">
+              <Button variant="contained" autoFocus type="submit" size="large">
                 Login
               </Button>
-              <Button variant="outlined" onClick={handleCloseReset} size = 'large'>
+              <Button
+                variant="outlined"
+                onClick={handleCloseReset}
+                size="large"
+              >
                 Cancel
               </Button>
             </Stack>
-            <Typography color = "primary.light">{validateInput}</Typography>
+            <Typography color="primary.light">{validateInput}</Typography>
             <Stack></Stack>
           </Stack>
         </Box>
-      </Card>
+      </Card> */}
     </Dialog>
   );
 }
--- website/src/pages/SignUp/ProgressStepper.jsx
+++ website/src/pages/SignUp/ProgressStepper.jsx
@@ -1,14 +1,12 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import AccountDetails from './AcccountDetails';
-import CustomizeSettings from './CustomizeSettings';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import AccountDetails from "./AccountDetails";
+import CustomizeSettings from "./CustomizeSettings";
 
-const steps = ['Enter Account Details', 'Customize Settings', 'Done!'];
+const steps = ["Enter Account Details", "Customize Settings", "Done!"];
 
 export default function ProgressStepper() {
   const [activeStep, setActiveStep] = React.useState(0);
@@ -18,25 +16,25 @@ export default function ProgressStepper() {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep} >
+    <Box sx={{ width: "100%" }}>
+      <Stepper activeStep={activeStep}>
         {steps.map((label, index) => {
           const stepProps = {};
           const labelProps = {};
           return (
-            <Step key={label} {...stepProps}>
+            <Step key={index} {...stepProps}>
               <StepLabel {...labelProps}>{label}</StepLabel>
             </Step>
           );
         })}
       </Stepper>
-      {activeStep === 0 ? (
-        <AccountDetails activeStep = {activeStep} setActiveStep = {setActiveStep}/>
-      ) : activeStep === 1 ? (
-        <CustomizeSettings />
-      ) : (
-        <></>
-      )}
+      {
+        {
+          0: <AccountDetails activeStep={activeStep} handleNext={handleNext} />,
+          1: <CustomizeSettings />,
+          2: <></>,
+        }[activeStep]
+      }
     </Box>
   );
 }
