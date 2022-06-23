2a392c39f0113b1d43d6e7ecf62613f2a12cc29e
hansstanley <nusstuhss@gmail.com>
2022-06-24 07:55:28 +0800

    Tried to make sticky module panel for roadmap

--- website/src/pages/Roadmap/Roadmap.jsx
+++ website/src/pages/Roadmap/Roadmap.jsx
@@ -1,7 +1,18 @@
-import { Typography } from "@mui/material";
-import { useContext } from "react";
+import { Box, Paper, Typography } from "@mui/material";
+import { useContext, useEffect, useRef } from "react";
 import ModuleStack from "./ModuleStack";
+import { ModuleStack as ModStack } from "../../components/Mod";
+import { ResponsiveStack } from "../../components";
 
 export default function Roadmap() {
-  return <ModuleStack />;
+  const modStack = useRef(null);
+
+  return (
+    <ResponsiveStack>
+      <Paper sx={{ p: 2, position: "sticky", top: 0, left: 0, zIndex: 3000 }}>
+        <ModStack />
+      </Paper>
+      <ModuleStack />
+    </ResponsiveStack>
+  );
 }
