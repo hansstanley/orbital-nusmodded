53adee1ba34e1e50cef1619b10d2b6d8598b9121
hansstanley <nusstuhss@gmail.com>
2022-07-26 00:09:53 +0800

    Update Landing.jsx

--- website/src/pages/Landing/Landing.jsx
+++ website/src/pages/Landing/Landing.jsx
@@ -1,9 +1,11 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   Avatar,
   Button,
   CircularProgress,
+  Fab,
   IconButton,
+  Tooltip,
   Typography,
   useTheme,
 } from "@mui/material";
@@ -11,49 +13,57 @@ import { useNavigate } from "react-router-dom";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import { useAuthSession } from "../../providers";
 import background from "../../res/wallpaper.jpg";
 
+const PAGE_COUNT = 2;
+
 export default function Landing() {
   const navigate = useNavigate();
-  const theme = useTheme();
   const { loading, isAuth } = useAuthSession();
   const parallax = useRef(null);
+  const [page, setPage] = useState({ value: 0 });
+
+  useEffect(() => {
+    parallax.current?.scrollTo(page.value);
+  }, [page, parallax]);
 
   const handleStart = () => {
     navigate(isAuth() ? "/roadmap" : "/signup");
   };
 
-  const handleScrollUp = () => parallax.current.scrollTo(0);
-  const handleScrollDown = () => parallax.current.scrollTo(1);
+  const handleScrollUp = () =>
+    setPage(({ value }) => ({ value: Math.max(value - 1, 0) }));
+  const handleScrollDown = () =>
+    setPage(({ value }) => ({ value: Math.min(value + 1, PAGE_COUNT - 1) }));
+  const handleScrollToTop = () => setPage({ value: 0 });
 
   return (
-    <Parallax
-      ref={parallax}
-      pages={2}
-      enabled={!loading}
-      style={{ top: 0, left: 0 }}
-    >
-      <ParallaxBackground />
-      <ParallaxTitle />
-      <ParallaxDown loading={loading} onClick={handleScrollDown} />
-      <ParallaxSlogan />
-      <ParallaxLayer
-        offset={1}
-        speed={2}
-        onClick={handleScrollUp}
-        style={styles.centered}
+    <>
+      <Parallax
+        ref={parallax}
+        pages={PAGE_COUNT}
+        enabled={!loading}
+        style={{ top: 0, left: 0 }}
       >
-        <Button
-          variant="contained"
-          onClick={handleStart}
-          endIcon={<ArrowForwardIcon />}
-          sx={{ mt: 20 }}
-        >
-          {isAuth() ? "To roadmap" : "Get started"}
-        </Button>
-      </ParallaxLayer>
-    </Parallax>
+        <ParallaxBackground />
+        <ParallaxTitle />
+        <ParallaxDown loading={loading} onClick={handleScrollDown} />
+        <ParallaxSlogan />
+        <ParallaxLayer offset={1} speed={2} style={styles.centered}>
+          <Button
+            variant="contained"
+            onClick={handleStart}
+            endIcon={<ArrowForwardIcon />}
+            sx={{ mt: 20 }}
+          >
+            {isAuth() ? "To roadmap" : "Get started"}
+          </Button>
+        </ParallaxLayer>
+        <ParallaxFab onClick={handleScrollToTop} />
+      </Parallax>
+    </>
   );
 }
 
@@ -83,8 +93,6 @@ function ParallaxBackground() {
 }
 
 function ParallaxTitle() {
-  const theme = useTheme();
-
   return (
     <>
       <ParallaxLayer offset={0} speed={2.5} style={styles.centered}>
@@ -188,6 +196,26 @@ function ParallaxDown({ onClick = () => {}, loading = false }) {
         </IconButton>
       </ParallaxLayer>
     </>
+  );
+}
+
+function ParallaxFab({ onClick = () => {} }) {
+  return (
+    <ParallaxLayer
+      offset={1.9}
+      factor={0.1}
+      speed={0.5}
+      style={{ display: "flex" }}
+    >
+      <Tooltip title="Back to top">
+        <Fab
+          onClick={onClick}
+          sx={{ position: "absolute", right: 32, bottom: 32 }}
+        >
+          <ArrowUpwardIcon />
+        </Fab>
+      </Tooltip>
+    </ParallaxLayer>
   );
 }
 
